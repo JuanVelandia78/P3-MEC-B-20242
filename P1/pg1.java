256b95e0585363a5e60865ff1f344689351ee4a0
JuanVelandia78 <est.juand.velandia@unimilitar.edu.co>
2024-08-27 14:38:15 -0500

    Add files via upload

---
+++ P1/pg1.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.parcialpractica;
+
+public class pg1 {
+     
+     public static void main(String[] args) {
+        int denominador;
+        int numerador;
+        
+    } 
+}

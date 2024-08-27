20a644ae1f3f7ea8bf9e09b5a55102c5ff4cfb76
JuanVelandia78 <est.juand.velandia@unimilitar.edu.co>
2024-08-27 15:02:45 -0500

    Add files via upload

---
+++ P1/pg3.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.parcialpractica;
+
+import java.util.Scanner;
+
+public class pg3 {
+    public class ParcialPractica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+       
+    }
+    }
+    public class fraccion{
+        float num1 = 7/8;
+        float den2 = 5/3;
+        float resultado = num1/den2;
+    }
+   
+   
+   
+    
+}
+
